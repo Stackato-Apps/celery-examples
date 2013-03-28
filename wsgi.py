3e3ec53bd44d73a5c2591f96cc26b9e61bb2f643
Matthew Fisher <matt.fisher6@gmail.com>
2013-03-28 11:16:03 -0700

    Revert "Update wsgi.py"
    This reverts commit 2a00598d783b1097f7b82f4c369b7c921c66f45a.

--- wsgi.py
+++ wsgi.py
@@ -15,7 +15,7 @@ def hello_world_app(environ, start_response):
     pyver = '.'.join(map(str, tuple(sys.version_info)[:3]))
 
     # The returned object is going to be printed
-    return ["Please run 'stackato run celery-examples python demo.py'." % pyver]
+    return ["Hello World (from Python %s WSGI)" % pyver]
     
 # For gunicorn
 application = hello_world_app
